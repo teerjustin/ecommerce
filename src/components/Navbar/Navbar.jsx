db16931cfc228faa87d56ee294800b39b5976085
teerjustin <teerjustin@gmail.com>
2021-10-03 00:52:07 -0700

    initialized navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,29 @@ import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '
 import { ShoppingCart } from '@material-ui/icons'
 import { classes } from 'istanbul-lib-coverage'
 
+import logo from '../../assets/market.png'
+import useStyles from './styles'
+
 const Navbar = () => {
+    const classes = useStyles();
+
     return (
         <div>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inhereit">
-                        <img src={} alt="Justin's Shop" height="25px" className={classes.image} />
+                        <img src={logo} alt="Justin's Shop" height="25px" className={classes.image} />
                         Justin's Shop
                     </Typography>
+                    <div className={classes.grow}/>
+
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show Cart Items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles'
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Streetwear Shoes', price: '$170', image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-UNC-Leather.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606322077'},
@@ -10,9 +11,11 @@ const products = [
 ]
 
 const Products = ({ product }) => {
+    const classes = useStyles();
 
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => ( 
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
