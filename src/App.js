df3f5f3457dad6765fb524fe21535fae2a02070d
teerjustin <teerjustin@gmail.com>
2021-10-03 01:38:21 -0700

    Initialized commerce API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Products from './components/Products/Products'
 import Navbar from './components/Navbar/Navbar'
+import { commerce } from './lib/commerce'
 
 
 
 const App = () => {
+    const [ products, setProducts ] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    console.log(products);
+
     return (
         <div>
             <Navbar/>
