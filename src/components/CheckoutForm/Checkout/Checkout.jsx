67db02783df219e77d2cfed9533ca14e78ab1d2e
teerjustin <teerjustin@gmail.com>
2021-10-13 15:22:03 -0700

    Created Address and Payment form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
 
 import useStyles from './style'
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping Address', 'Payment details'];
 
@@ -10,20 +12,30 @@ const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
 
-    
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    )
+
+    const Form = () => activeStep === 0
+        ? <AddressForm/>
+        : <PaymentForm/>
+
     return (
         <>
         <div className={classes.toolbar} />
         <main className={classes.layout}>
             <Paper className={classes.paper}>
-            <Typography variant="h4" align="center">Checkout</Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
-                {steps.map((label) => (
-                <Step key={label}>
-                    <StepLabel>{label}</StepLabel>
-                </Step>
-                ))}
-            </Stepper>
+                <Typography variant="h4" align="center">Checkout</Typography>
+                <Stepper activeStep={activeStep} className={classes.stepper}>
+                    {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                    ))}
+                </Stepper>
+                {activeStep === steps.length ? <Confirmation/> : <Form />}
             </Paper>
         </main>
         </>
