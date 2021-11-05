a7731834808335e8caf5510216efb937194fffa4
teerjustin <teerjustin@gmail.com>
2021-11-04 18:05:44 -0700

    initialized stripe

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -5,10 +5,66 @@ import { loadStripe } from '@stripe/stripe-js'
 import Review from './Review'
 
 
-const PaymentForm = ({ checkoutToken}) => {
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+
+
+const PaymentForm = ({ checkoutToken, backStep}) => {
+    const handleSubmit = (event, elements, stripe) => {
+        event.preventDefault();
+
+        if(!stripe || !elements) return;
+
+        const cardElement = elements.getElement(CardElement);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({ type:'card', card: cardElement});
+
+        if (error) {
+            console.log(error);
+
+        } else {
+            const orderData = {
+                line_items: checkoutToken.live.line_items,
+                customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
+                shipping: { 
+                    name: 'Primary', 
+                    street: shippingData.address1,
+                    town_city: shippingData.city, 
+                    county_state: shippingData.shippingSubDivision,
+                    postal_zip_code: shippingData.zip,
+                    country: shippingData.shippingCountry,
+                },
+                fulfillment: { shipping_method: shippingData.shippingOption },
+                payment: {
+                    gateway: 'stripe',
+                    stripe: {
+                        payment_method_id: paymentMethod.id
+                    }
+                }
+            }
+        }
+    }
     return (
         <>
             <Review checkoutToken={checkoutToken}/>
+            <Divider />
+            <Typography variant="h6" gutterbottom style={{ margin: '20px 0' }}> Payment Method</Typography>
+            <Elements stripe={stripePromise}> 
+                <ElementsConsumer>
+                    {({ elements, stripe }) => (
+                        <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
+                            <CardElement/>
+                            <br /> <br/>
+                            <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                                <Button variant="outlined" onClick={backStep}> Back </Button>
+                                <Button type="submit" variant="contained" disabled={!stripe} color="primary"> 
+                                    Pay { checkoutToken.live.subtotal.formatted_with_symbol }
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+
+            </Elements>
         </>
     );
 }
